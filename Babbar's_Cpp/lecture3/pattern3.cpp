1e74fb363fad2d20152fd9638b795f35944a4a4d
CodingNinja576 <576divyanshsharma@gmail.com>
2025-03-23 23:06:18 +0530

    "commmit"

---
+++ Babbar's_Cpp/lecture3/pattern3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int n =4;
+  int i = 1;
+  while(i<=n){
+    int j =1;
+    // int k = 1;
+    while(j<=n){
+      cout<<j<<" ";
+  
+      // cout<<k<<" ";
+      // k=k+1;
+      j=j+1;
+
+  }
+  i++;
+  cout<<endl;
+
+  }
+}
