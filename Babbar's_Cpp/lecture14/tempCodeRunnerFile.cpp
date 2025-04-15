740c7d82b1f1168aa5d475bb08c5a1533ddbfcc4
CodingNinja576 <576divyanshsharma@gmail.com>
2025-04-16 00:22:44 +0530

    "commit"

---
+++ Babbar's_Cpp/lecture14/tempCodeRunnerFile.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int findpivot(int arr[],int size){
+  int start =0;
+  int end = size-1;
+  int mid = start + (end - start)/2;
+  while(start<end){
+    if(arr[mid]>=arr[0]){ 
+      start = mid+1; // 1st line ki condition
+    }
+    else{
+      end = mid;  // 2nd line ki condition
+    }
+    mid = start + (end - start)/2;
+  }
+  return start;
+}
+int main(){
+  int arr[5]={2,8,10,1,3};
+  int pivot = findpivot(arr,5);
+  cout<<"pivot of the array is "<< pivot<<endl;
+}
