1d174c2f1439fe51be2d922b18a9c11605a4724f
CodingNinja576 <576divyanshsharma@gmail.com>
2025-06-05 22:26:24 +0530

    "push "

---
+++ Babbar's_Cpp/contest 1/Apple Shop with Ninja.cpp
@@ -0,0 +1,15 @@
+// long long appleAndCoupon(int n, int m, vector<int> arr){
+//     // Write your code here.long long appleAndCoupon(int n, int m, vector<int> arr){
+//     // for(int i =1 ; i < n ; i ++){
+//     //     for ( int j = 0 ; j )
+//     // }
+//     sort(arr.begin(),arr.end());
+//     int sum = 0 ;
+//     for (int i = 0;i < n ; i ++){
+//         sum +=arr[i];
+//     }
+//     int cost= sum - arr[n-m];
+//     return cost;
+
+
+// }
---
+++ Babbar's_Cpp/lecture20/merge.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+void main() {
+
+ int arr1[6]={1,3,5,7,9,11};
+ int arr2[5]= {2,4,6,8,10};
+
+
+
+}
